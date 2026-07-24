72327946341174d6bae553d98acad00997fb4103
ShubhamGautam7 <proshubham764@gmail.com>
2026-07-24 21:22:36 +0545

    uploaded vending machine program

---
+++ Exercises/Loops/vending machine.py
@@ -0,0 +1,13 @@
+
+amount_due = 50
+
+while amount_due > 0:
+    print(f"Amount Due: {amount_due}")
+    coin = int(input("Incert Coin: "))
+
+    if coin in [25, 10, 5, 1]:
+        amount_due -= coin
+            
+print(f"Change owed: {abs(amount_due)}")
+
+
